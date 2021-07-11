0423ef80f635f7b72e7e85982297d470d26d74b1
Anthony Cecconato <antho.cecc@gmail.com>
2021-07-11 10:18:15 +0200

    WIP on default controllers

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,9 +1,22 @@
-const usersModel = require('../models/users.model');
+const Users = require('../models/users.model');
+const processMongooseError = require('../services/processMongooseError');
 
 exports.create = (req, res) => {
-  res.status(201).json({ message: 'Created' });
+  const newUser = new Users(req.body);
+  newUser.save()
+    .then((data) => {
+      res.status(201).json({ message: `User ${data._id} created` });
+    })
+    .catch((error) => {
+      processMongooseError(error, res);
+    });
 };
 
 exports.findAll = (req, res) => {
-  res.json({ message: 'findAll' });
+  Users.find()
+    .then((data) => {
+      res.json({ data });
+    }).catch((error) => {
+      processMongooseError(error, res);
+    });
 };
