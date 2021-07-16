64bc5dacf36402625132d04afec56fee91569b54
Anthony Cecconato <antho.cecc@gmail.com>
2021-07-16 09:56:39 +0200

    Remove routes versioning (doesn't work)

--- app/routes/sauces.routes.js
+++ app/routes/sauces.routes.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const routesVersioning = require('express-routes-versioning')();
 
 const router = express.Router();
 
 const saucesController = require('../controllers/sauces.controller');
 
-router.get('/', routesVersioning({
-  '1.0': saucesController.findAll,
-}));
+router.get('/', saucesController.findAll);
 
-router.post('/', routesVersioning({
-  '1.0': saucesController.create,
-}));
+router.post('/', saucesController.create);
 
 module.exports = router;
--- app/routes/users.routes.js
+++ app/routes/users.routes.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const routesVersioning = require('express-routes-versioning')();
 
 const router = express.Router();
 
 const usersController = require('../controllers/users.controller');
 
-router.get('/', routesVersioning({
-  '1.0': usersController.findAll,
-}));
+router.get('/', usersController.findAll);
 
-router.post('/', routesVersioning({
-  '1.0': usersController.create,
-}));
+router.post('/', usersController.create);
 
 module.exports = router;
