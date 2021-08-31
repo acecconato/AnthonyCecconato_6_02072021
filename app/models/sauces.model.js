06e16b66f4d3727596c00d85134a65b04645afdf
Anthony Cecconato <antho.cecc@gmail.com>
2021-08-31 20:31:14 +0200

    Add unique validation on sauces name

--- app/models/sauces.model.js
+++ app/models/sauces.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const saucesSchema = new mongoose.Schema({
   userId: {
@@ -6,7 +7,7 @@ const saucesSchema = new mongoose.Schema({
   },
 
   name: {
-    type: String, required: true, maxlength: 30, trim: true,
+    type: String, required: true, maxlength: 30, trim: true, unique: true,
   },
 
   manufacturer: {
@@ -49,5 +50,8 @@ const saucesSchema = new mongoose.Schema({
     type: Number, trim: true, default: 0,
   },
 });
+
+// Prettier error message on unique error
+saucesSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('sauces', saucesSchema);
